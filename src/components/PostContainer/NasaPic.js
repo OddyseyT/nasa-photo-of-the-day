3094bc6b51a74723ec3bd68b4abb4f99062fd05d
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-28 21:55:47 -0500

    Sixth push.  Added reactstrap card compnent

--- src/components/PostContainer/NasaPic.js
+++ src/components/PostContainer/NasaPic.js
@@ -1,25 +1,44 @@
 import React from "react";
-
+import { Container, Card, CardHeader, CardFooter, CardBody,
+ CardText } from 'reactstrap';
 
 import "./Post.css";
 
 const NasaPic = props => {
     console.log(props)
 
-    return (
-        <div className="posts-container-wrapper">
-      <h2>{props.title}</h2>
+   return (
+    //     <div className="posts-container-wrapper">
+    //   <h2>{props.title}</h2>
+    //   <img
+    //       alt="post thumbnail"
+    //       className="post-image"
+    //       src={props.Picture}
+    //     />
+    //   <div className="bottom">
+    //     <p>{props.Explanation}</p>
+    //     <p>{`Copyright: ${props.Copyright}`}</p>
+    //     <p>{`Date: ${props.Date}`}</p>
+    //   </div>
+    // </div>
+
+<Card>
+      <CardHeader>{props.title}</CardHeader>
+      <CardBody>
       <img
           alt="post thumbnail"
-          className="post-image"
-          src={props.Picture}
-        />
-      <div className="bottom">
-        <p>{props.Explanation}</p>
-        <p>{`Copyright: ${props.Copyright}`}</p>
-        <p>{`Date: ${props.Date}`}</p>
-      </div>
-    </div>
+         className="post-image"
+         src={props.Picture}
+       />
+        
+        <CardText>{props.Explanation}</CardText>
+      {/* //  <Button>Go somewhere</Button> */}
+      </CardBody>
+      <CardFooter>
+      <p>{`Copyright: ${props.Copyright}`}</p>
+     <p>{`Date: ${props.Date}`}</p>
+      </CardFooter>
+    </Card>
 
     )
     }
